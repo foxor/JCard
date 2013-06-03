17e81912804f288903cfc8ba87b8e71d8a32d4bb
Isaac James <izach.james@gmail.com>
2013-06-03 14:08:25 -0700

    Property can now reference properties of non-context objects, and access propertied in metadata

--- src/main/java/com/foxor/jcard/geml/expressions/Property.java
+++ src/main/java/com/foxor/jcard/geml/expressions/Property.java
@@ -5,11 +5,12 @@ import java.lang.reflect.Method;
 import org.apache.commons.lang3.StringUtils;
 
 import com.foxor.jcard.geml.Expression;
+import com.foxor.jcard.geml.GObject;
 import com.foxor.jcard.geml.Machine;
 
 /**
  * 
- * Property gets a property from the context GObject
+ * Property gets a property from the context GObject, or a specified local object
  * 
  * Server only
  * 
@@ -19,9 +20,22 @@ import com.foxor.jcard.geml.Machine;
 public class Property extends Expression {
     
     /**
+     * The object to query
+     */
+    protected GObject source;
+    
+    /**
      * The name of the property to retrieve.
      */
     protected String name;
+
+    public GObject getSource() {
+        return source;
+    }
+
+    public void setSource(GObject source) {
+        this.source = source;
+    }
 
     public String getName() {
         return name;
@@ -33,7 +47,20 @@ public class Property extends Expression {
 
     @Override
     public Expression execute(Machine m) throws Exception {
-        Method method = m.getContext().getClass().getMethod("get" + StringUtils.capitalize(name), new Class<?>[0]);
-        return (Expression)method.invoke(m.getContext(), new Object[0]);
+        GObject sourceBackup = source;
+        if (source == null) {
+            source = m.getContext();
+        }
+        GObject self = (GObject)source.execute(m);
+        Object val;
+        if (self.getData() != null && self.getData().containsKey(getName())) {
+            val = self.getData().get(getName());
+        }
+        else {
+            Method method = self.getClass().getMethod("get" + StringUtils.capitalize(name), new Class<?>[0]);
+            val = method.invoke(self, new Object[0]);
+        }
+        source = sourceBackup;
+        return Type.TypeBox(val);
     }
 }
